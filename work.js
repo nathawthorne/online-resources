02ea87f50c70ad37f1560836b0f957e11a5d40a9
nathawthorne <62619111+nathawthorne@users.noreply.github.com>
2020-03-25 00:25:30 -0500

    Adding more modularity

---
+++ work.js
@@ -0,0 +1,63 @@
+var teacher_work = {
+Ana_Fernandez:{'teacher_name': 'Ana Fernandez', 'grade': 'K-B', 'work': ['text__Watch the following video for support on the homework!', 'video__head_moving.mp4', 'text__Thanks for watching!']},
+Miguel_Del_Rio:{'teacher_name': 'Miguel Del Rio', 'grade': 'K-A', 'work': ['text__Alpha Go Documentary', 'link__https://www.youtube.com/watch?v=WXuK6gekU1Y&feature=youtu.be', 'text__Please watch the first 10 minutes of this video. Write your response and send it to me via email: test@gmail.com.']},
+Narciso_Del_Rio:{'teacher_name': 'Narciso Del Rio', 'grade': 'K-C', 'work': ['text__No work today']}}
+
+
+
+//##
+
+special_text = 'text__';
+special_video = 'video__';
+special_link = 'link__';
+
+function extract_var(name){
+	var url = window.location.href
+	var first_q = url.indexOf('html?')
+
+	var all_vars = url.substring(first_q+1, url.length)
+
+	var useful_var = all_vars.indexOf(name)
+	var next_var = all_vars.indexOf('?',useful_var)
+	if (next_var == -1){
+		next_var = all_vars.length
+	}
+
+	var value = all_vars.substring(useful_var + name.length+1,next_var)
+
+	return value
+}
+
+function string_to_var(name){
+	return name.replace(/ /g, "_");
+}
+
+$(document).ready(function(){
+	teacher_name = extract_var('teacher');
+	var_name = string_to_var(teacher_name);
+	json_document = teacher_work[teacher_name];
+	html_doc = '<h1>'+json_document['teacher_name']+'</h1>'
+	html_doc += '<h1>'+json_document['grade']+'</h1><br>'
+
+	all_work = json_document['work'];
+	console.log(all_work);
+	for(var work_id = 0; work_id < all_work.length; work_id++){
+		if(all_work[work_id].startsWith(special_text)){
+			text = all_work[work_id].substring(special_text.length);
+
+			html_doc += '<h3>'+text+'</h3><br>'
+		}
+		else if(all_work[work_id].startsWith(special_link)){
+			link = all_work[work_id].substring(special_link.length);
+			html_doc+='<a class="link" href="'+link+'"><h3>Click Here</h3></a><br>'
+		}
+		else if(all_work[work_id].startsWith(special_video)){
+			link = all_work[work_id].substring(special_video.length);
+			link = 'teachers/'+var_name+'/'+link
+			html_doc+='<video controls><source src="'+link+'" type="video/mp4">There was an issue loading the video...</video><br>'
+		}
+	}
+	lastbr = html_doc.lastIndexOf('<br>')
+	html_doc = html_doc.substring(0, lastbr)
+	$('.work_container').html(html_doc);
+});
---
+++ write_work_jsons.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+special_remove = '//##'
+json_folder_loc = 'teachers'
+
+ignore_set = set(['.DS_Store'])
+
+json_files = []
+for root, dirs, files in os.walk(json_folder_loc, topdown=False):
+    for name in files:
+        if '.json' in name:
+            json_files.append(os.path.join(root, name))
+
+# generate string
+to_write = "var teacher_work = {\n"
+for f in json_files:
+    with open(f) as json_file:
+        data = json.load(json_file)
+        key = data['teacher_name']
+        print(key)
+        key = key.replace(' ', '_')
+        to_write += key + ':' + str(data) + ',\n'
+to_write = to_write.strip()
+to_write = to_write[:len(to_write)-1]
+to_write += "}\n\n"
+
+
+# clear out old var
+with open('work.js') as work_file:
+    data = ""
+    for line in work_file:
+        data += line
+
+remove_index = max(data.index(special_remove) - 2, 0)
+
+data = data[remove_index:]
+data = to_write + data
+
+with open('work.js', 'w+') as work_file:
+    work_file.write(data)
