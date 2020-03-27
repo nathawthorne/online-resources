86be32f8436ac92544decf7741ee462f9e2b7b9c
Miguel Del Rio <drmiguel@mit.edu>
2020-03-27 16:03:28 -0500

    adding new links and fixing font

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,11 @@
 var text_files = {
-"main_page":{'image': 'imgs/anfernandez.jpg', 'text': {'en': 'Hello Hawthorne Families!<br /><br />Here are some important links for you to use at home to support your students, every week starting on Monday March 23rd, we will add more activities and ideas.', 'es': 'Hola Familias de Hawthorne,<br /><br />En esta pagina, cada semana empezando el lunes 23 de marzo, pondremos ideas y actividades que ustedes pueden usar para ayudar a sus hijos en casa.'}, 'links_title': {'en': 'Helpful Links', 'es': 'Apoyo Electronico'}, 'links': ['www.google.com', 'www.bing.com'], 'links_text': ['Google', 'Bing']},
+"main_page":{'image': 'imgs/anfernandez.jpg', 'text': {'en': 'Hello Hawthorne Families!<br /><br />Here are some important links for you to use at home to support your students, every week starting on Monday March 23rd, we will add more activities and ideas.', 'es': 'Hola Familias de Hawthorne,<br /><br />En esta pagina, cada semana empezando el lunes 23 de marzo, pondremos ideas y actividades que ustedes pueden usar para ayudar a sus hijos en casa.'},
+'links_title': {'en': 'Useful Resources', 'es': 'Recursos Utiles'}, 'food_bank_links': ['https://www.citysquare.org/food', 'https://www.ivcompassion.org/', 'https://goodstreetbaptistchurch.org/social-service-center-2/'], 'food_bank_links_text': ['City Square Food Pantry', 'Inspired Vision Compassion Center', 'Good Street Baptist Church'], 'food_bank_extra_info':{'en':['Tuesday - Friday 9:00 am - 1 pm,<br />1610 S Malcolm X Blvd, Bldg 350, Dallas, TX 75226<br />(214) 823-8710' ,'Monday - Friday 9:30 am - 5:00 pm, <br/>2019 N. Masters, Dallas, Texas 75217, <br/>P:(972) 971-8879' ,'Monday, Tuesday, Friday 10:00 am - 12:00 pm,<br />Wednesday 12:00 pm - 2:00 pm,<br />Thursday 10:00 am - 2:00 pm,<br />3126 Hatcher Street, Dallas, TX 75215<br />(214) 421-8208'],
+'es':['Martes - Viernes 9:00 am - 1 pm,<br />1610 S Malcolm X Blvd, Bldg 350, Dallas, TX 75226<br />(214) 823-8710' ,'Lunes - Viernes 9:30 am - 5:00 pm, <br/>2019 N. Masters, Dallas, Texas 75217, <br/>P:(972) 971-8879','Lunes, Martes, Viernes 10:00 am - 12:00 pm,<br /> Miercoles 12:00 pm - 2:00 pm,<br />Jueves 10:00 am - 2:00 pm,<br />3126 Hatcher Street, Dallas, TX 75215<br />(214) 421-8208']},'shelter_links':['https://www.familygateway.org/', 'https://www.edenoutreach.com/contactus.htm', 'https://hccdallas.org/'],'shelter_text':['Family Gateway', 'Eden Outreach', 'Housing Crisis Center'],'shelter_extra_info':['711 S. St. Paul St. Dallas, TX 75201<br />(214) 823-4500', '3110 Metropolitan Ave, Dallas, Texas 75215<br />(214) 421-5894', '4210 Junius Street, Dallas, TX 75246<br />(214) 828-4244']},
 "menu_text":{'en': {'prek': 'Pre-K', 'K': 'Kindergarten', 'first': 'First Grade', 'second': 'Second Grade', 'third': 'Third Grade', 'fourth': 'Fourth Grade', 'fifth': 'Fifth Grade', 'specials': 'Specials'}, 'es': {'prek': 'Pre-K', 'K': 'Kinder', 'first': 'Primer Grado', 'second': 'Segundo Grado', 'third': 'Tercer Grado', 'fourth': 'Cuarto Grado', 'fifth': 'Quinto Grado', 'specials': 'Especiales'}}}
 
 
 
-//##
 
 
 function remove_page(key){
@@ -51,16 +52,49 @@ function setup_page(language){
 	$('.main_text').html('<p>'+text_files['main_page']['text'][language]+'</p>');
 
 	// setting up links
-	var links = text_files['main_page']['links'];
-	var link_text = text_files['main_page']['links_text'];
-	to_write = "<h4>"+text_files['main_page']['links_title'][language]+"</h4>"
-	for(var link_id = 0; link_id < links.length; link_id++){
-		var txt = link_text[link_id];
-		var src = links[link_id];
+	to_write = "<h4>"+text_files['main_page']['links_title'][language]+"</h4><ul>"
+	$('.links_title').html(to_write);
 
-		to_write += '<a href="'+src+'">'+txt+'</a>&nbsp;&nbsp;&nbsp;&nbsp;'
+	var food_bank_links = text_files['main_page']['food_bank_links'];
+	var food_bank_links_text = text_files['main_page']['food_bank_links_text'];
+	var food_bank_extra_info = text_files['main_page']['food_bank_extra_info'][language];
+
+	if(language == 'en')
+	{
+		to_write = '<h5>Food Pantries</h5>';
+	} else {
+		to_write = '<h5>Despensas de Alimentos</h5>';
 	}
-	$('.links').html(to_write);
+	for(var link_id = 0; link_id < food_bank_links.length; link_id++){
+		var txt = food_bank_links_text[link_id];
+		var src = food_bank_links[link_id];
+		var extra = food_bank_extra_info[link_id];
+		to_write += '<li><a href="'+src+'">'+txt+'</a><br>'
+		to_write += extra+'</li><br>'
+	}
+	to_write += "</ul>"
+	$('.food_bank').html(to_write);
+
+	var shelter_links = text_files['main_page']['shelter_links'];
+	var shelter_links_text = text_files['main_page']['shelter_text'];
+	var shelter_extra_info = text_files['main_page']['shelter_extra_info'];
+
+	if(language == 'en')
+	{
+		to_write = '<h5>Shelter Assistance</h5>';
+	} else {
+		to_write = '<h5>Assistencia para Vivienda</h5>';
+	}
+	for(var link_id = 0; link_id < shelter_links.length; link_id++){
+		var txt = shelter_links_text[link_id];
+		var src = shelter_links[link_id];
+		var extra = shelter_extra_info[link_id];
+		to_write += '<li><a href="'+src+'">'+txt+'</a><br>'
+		to_write += extra+'</li><br>'
+	}
+	to_write += "</ul>"
+	console.log(to_write)
+	$('.shelter').html(to_write);
 }
 
 $(document).ready(function(){
